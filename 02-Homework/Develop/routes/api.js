000fecbd370773a91d8a386a33ce9b34b45b6ed6
gjpataky <gjpataky@yahoo.com>
2020-01-23 19:16:28 -0600

    initial commit

---
+++ 02-Homework/Develop/routes/api.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const Workout = require("./../models/workout");
+/* add a new workout */
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+/* add excercises to a work out */
+/* put route /api/workouts/:id */
+/* get all workouts */
+/* /api/workouts */
+/* get last 7 workout */
+/*  /api/workouts/range */
+/* delete workout */
+/* /api/workouts */
+module.exports = router;
