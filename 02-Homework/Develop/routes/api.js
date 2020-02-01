2054e5a76878e57ab9b805ec81de98a0999bf751
gjpataky <gjpataky@yahoo.com>
2020-02-01 16:01:58 -0600

    commit

--- 02-Homework/Develop/routes/api.js
+++ 02-Homework/Develop/routes/api.js
@@ -19,3 +19,58 @@ router.post("/api/workouts", (req, res) => {
 /* delete workout */
 /* /api/workouts */
 module.exports = router;
+
+/* put route /api/workouts/:id */
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    // "runValidators" will ensure new exercises meet our schema requirements
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+/* get all workouts */
+/* /api/workouts */
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+/* get last 7 workout */
+/*  /api/workouts/range */
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then(dbWorkouts => {
+      console.log(dbWorkouts);
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+/* delete workout */
+/* /api/workouts */
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+module.exports = router;
