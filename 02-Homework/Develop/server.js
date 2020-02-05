ad99af2446a199cb83ada86f89e64d3b420c0836
gjpataky <gjpataky@yahoo.com>
2020-02-04 21:36:15 -0600

    heroku2

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -1,31 +1,18 @@
 const express = require("express");
 const logger = require("morgan");
-var mongoose = require("mongoose");
-var MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/workouts";
-const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4 // Use IPv4, skip trying IPv6
-};
-
-const PORT = 3000;
-
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
-mongoose.connect(MONGODB_URI, options);
-
-// routes
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+});
 app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
-
+app.use(require("./routes/view.js"));
+// Start the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
